9f7f7d9672978fd9a37b302795b422dd8e37ade8
sanskar jain <sanskarj15@gmail.com>
2024-12-14 00:21:24 +0530

    enum any and unknown

--- dist/index.js
+++ dist/index.js
@@ -13,3 +13,18 @@ var UserRoles;
     UserRoles["GUEST"] = "guest";
     UserRoles["SUPER_ADMIN"] = "super_admin";
 })(UserRoles || (UserRoles = {}));
+// Any type
+let a = 12; // we have defined type explicitly
+let b = "string"; // any types basically lets a varaible to
+b = 12; //have any data type which is not good
+b = true;
+b.toUpperCase(); // here this will not give any error because any is like typescript off but in the case of unknown we will get error
+//unknown
+let x;
+x = "string";
+x = 12;
+//x.toUpperCase(); // this will give error
+//so in the case of unknown we will have to check for the type first
+if (typeof x == "string") {
+    x.toUpperCase();
+}
--- src/index.ts
+++ src/index.ts
@@ -15,3 +15,24 @@ enum UserRoles {
   GUEST = "guest",
   SUPER_ADMIN = "super_admin",
 }
+
+// Any type
+let a: number = 12; // we have defined type explicitly
+
+let b: any = "string"; // any types basically lets a varaible to
+b = 12; //have any data type which is not good
+b = true;
+
+b.toUpperCase(); // here this will not give any error because any is like typescript off but in the case of unknown we will get error
+
+//unknown
+let x: unknown;
+x = "string";
+x = 12;
+
+//x.toUpperCase(); // this will give error
+//so in the case of unknown we will have to check for the type first
+
+if (typeof x == "string") {
+  x.toUpperCase();
+}
