f7a3030a7ef6258cc0c371da8ffb8edfc07de217
sanskar jain <sanskarj15@gmail.com>
2024-12-14 15:20:10 +0530

    classes and constructurs

--- dist/index.js
+++ dist/index.js
@@ -16,9 +16,9 @@ var UserRoles;
 // Any type
 let a = 12; // we have defined type explicitly
 let b = "string"; // any types basically lets a varaible to
+b.toUpperCase(); // here this will not give any error because any is like typescript off but in the case of unknown we will get error
 b = 12; //have any data type which is not good
 b = true;
-b.toUpperCase(); // here this will not give any error because any is like typescript off but in the case of unknown we will get error
 //unknown
 let x;
 x = "string";
@@ -43,4 +43,44 @@ j = 12; //this wont give error because we did not explicitly define the type to 
 //undefined
 let m = undefined;
 // never
-// something which will never return anything
+// something which will never return anything or will never run
+// like codes written after return statement
+// A function returning 'never' cannot have a reachable end point
+function sayHi() {
+    while (true) { }
+}
+// Type inference -> Basically when you do not give a type and TS by itself infer or get a type for that vairable
+let z = "hello";
+// Type annotation -> when you explicitly give a type
+let y = 23;
+let r = "sanskar";
+let k;
+// Classes and Objects
+class Device {
+    constructor(name, price, category, year = 2025 //default value
+    ) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+}
+let d1 = new Device("lg", 1200, "digital");
+// one more way of making a constructor
+class Bird {
+    constructor(name, canSpeak, canFly) {
+        this.name = name;
+        this.canSpeak = canSpeak;
+        this.canFly = canFly;
+    }
+}
+let b1 = new Bird("Kiwi", true, false);
+console.log(b1);
+class Phone {
+    constructor(phoneNumber, brand, price = 12000) {
+        this.phoneNumber = phoneNumber;
+        this.brand = brand;
+        this.price = price;
+    }
+}
+let p1 = new Phone(123, "Samsung");
+// Access Modifiers
